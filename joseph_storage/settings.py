c3af7406f49c239531c05b1dd3b1d423d1a9385a
Stayman Hou <stayman.hou@gmail.com>
2017-04-13 20:46:03 -0400

    update setting

--- joseph_storage/settings.py
+++ joseph_storage/settings.py
@@ -30,7 +30,7 @@ else:
 #USER_AGENT = 'joseph_storage (+http://www.yourdomain.com)'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS=24
+CONCURRENT_REQUESTS=32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
